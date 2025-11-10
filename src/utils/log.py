7b3290b35b6a7794e5047da1ac335e0e1fd83a62
Alexander da Franca Fernandes <ext.a.fernandes@farfetch.com>
2025-11-10 10:57:17 +0000

    chore(logging): add logging layer utility

---
+++ src/utils/log.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+
+
+def init_logger(level=logging.INFO) -> logging.Logger:
+    """
+    Create the logging layer.
+    """
+    logger = logging.getLogger("clarityai")
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(level)
+    return logger
+
+
